ff24fc4b1df5796dcb29850babdd831a3f9c1634
Juan Larrondo <juan.larrondo@thinkia.com>
2026-03-23 20:07:59 +0100

    detalles

--- dwnld_image/main.tf
+++ dwnld_image/main.tf
@@ -82,3 +82,9 @@ resource "proxmox_virtual_environment_vm" "backend_node" {
 		}
 	}
 }
+
+
+output "vm_ipv4_addresses" {
+	description	= "Direcciones IP asignadas a las máquinas virtuales por DHCP"
+	value		= [for vm in proxmox_virtual_environment_vm.backend_node : vm.ipv4_addresses]
+}
